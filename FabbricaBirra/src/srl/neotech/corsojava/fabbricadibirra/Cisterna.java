0127f75419658e68422f96df667f5112d11d3885
Sofia <s.camponeschi@neotech.srl>
2022-11-02 11:07:22 +0100

    terza commit
    terzacommit

---
+++ FabbricaBirra/src/srl/neotech/corsojava/fabbricadibirra/Cisterna.java
@@ -0,0 +1,46 @@
+package srl.neotech.corsojava.fabbricadibirra;
+
+import java.util.Random;
+
+public class Cisterna {
+	
+	private Integer quantita;
+	private Integer evapPerDay;
+	private Integer livelloGas;
+	
+	public Integer getQuantita() {
+		return quantita;
+	}
+	public void setQuantita(Integer quantita) {
+		this.quantita = quantita;
+	}
+	public Integer getEvapPerDay() {
+		return evapPerDay;
+	}
+	public void setEvapPerDay(Integer evapPerDay) {
+		this.evapPerDay = evapPerDay;
+	}
+	public Integer getLivelloGas() {
+		return livelloGas;
+	}
+	public void setLivelloGas(Integer livelloGas) {
+		this.livelloGas = livelloGas;
+	
+	
+	for(int i=0; i<100; i++) {
+		
+	}
+	
+	Random rnd= Random();
+	
+	}
+	private Random Random() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
+
+	
+	
+	
+	
